4eb01eea9b709d0a73ffc1898fb532b65b3b88b6
yuanjiang-lance <lance682@qq.com>
2025-08-15 03:49:22 +0000

    Deploying to gh-pages from @ yuanjiang-lance/yuanjiang-lance.github.io@f6a93eb03ccc4a12c8ab27d94f0b924aa0a740a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,16 +412,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-tada-my-academic-website-is-now-live-stay-tuned-for-updates",
+          title: ':tada: My academic website is now live! Stay tuned for updates!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
